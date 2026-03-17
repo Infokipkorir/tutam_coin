95b6a098b0491f0bea8a61d9c00331a00f30d391
Gaatech solution <135028093+Infokipkorir@users.noreply.github.com>
2026-03-17 08:48:57 +0300

    Add function to load and display Bitcoin price

--- api.js
+++ api.js
@@ -1,1 +1,12 @@
+async function loadBTC(){
 
+let res = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd");
+
+let data = await res.json();
+
+document.getElementById("btcPrice").innerText = "$" + data.bitcoin.usd;
+
+}
+
+setInterval(loadBTC,5000);
+loadBTC();
