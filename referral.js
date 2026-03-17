25a308366c72a84fdfd6a77d2a206a6dac9d74e8
Gaatech solution <135028093+Infokipkorir@users.noreply.github.com>
2026-03-17 08:50:45 +0300

    Add referral code generation and application functions

--- referral.js
+++ referral.js
@@ -1,1 +1,17 @@
+function generateRef(uid){
+return uid.substring(0,6).toUpperCase();
+}
 
+function applyReferral(){
+
+let code = document.getElementById("refInput").value;
+
+if(!code){
+alert("Enter code");
+return;
+}
+
+alert("Referral applied! Bonus +500 coins");
+addCoins(500);
+
+}
